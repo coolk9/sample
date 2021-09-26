a5fdc41582ac10bead3d76dbcc2d804de10fdd97
Khushboo Ajmera <30569546+Jeanny1@users.noreply.github.com>
2021-09-26 18:45:24 +0530

    static changes

--- ajm/home/urls.py
+++ ajm/home/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
-admin.site.site_header = "Project Admin"
-admin.site.site_title = "Project Admin Portal"
-admin.site.index_title = "Welcome to Project Researcher Portal"
+admin.site.site_header = "Learn Admin"
+admin.site.site_title = "Learn Admin Portal"
+admin.site.index_title = "Welcome to Learning Researcher Portal"
 
 urlpatterns = [
-    path("", views.index, name='home'),
+    path("index", views.index, name='index'),
     path("home", views.home, name='home'),
     path("about", views.about, name='about'),
     path("services", views.services, name='services'),
